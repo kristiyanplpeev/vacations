0219025ed7b5e705453ffffda3be4211d5b34ddb
Ivelin Stoyanov <ivelin.stoyanov@atscale.com>
2021-10-29 10:20:42 +0300

    Refactor Homepage

--- client/src/components/Homepage/Homepage.tsx
+++ client/src/components/Homepage/Homepage.tsx
@@ -1,79 +1,19 @@
 import React, { Component } from "react";
 
 import { Button, Divider } from "@material-ui/core";
-import CircularProgress from "@material-ui/core/CircularProgress";
 import Dialog from "@material-ui/core/Dialog";
 import DialogTitle from "@material-ui/core/DialogTitle";
 import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
 import ListItemText from "@material-ui/core/ListItemText";
-import Paper from "@material-ui/core/Paper";
 import Snackbar from "@material-ui/core/Snackbar";
-import Table from "@material-ui/core/Table";
-import TableBody from "@material-ui/core/TableBody";
-import TableCell from "@material-ui/core/TableCell";
-import TableContainer from "@material-ui/core/TableContainer";
-import TableFooter from "@material-ui/core/TableFooter";
-import TableHead from "@material-ui/core/TableHead";
-import TableRow from "@material-ui/core/TableRow";
-import Typography from "@material-ui/core/Typography";
-import SentimentSatisfiedSharpIcon from "@material-ui/icons/SentimentSatisfiedSharp";
 import { Alert } from "@material-ui/lab";
-import MuiDateRangePickerDay, { DateRangePickerDayProps } from "@mui/lab/DateRangePickerDay";
-import StaticDateRangePicker from "@mui/lab/StaticDateRangePicker";
-import Badge from "@mui/material/Badge";
-import Box from "@mui/material/Box";
-import Grid from "@mui/material/Grid";
-import { styled } from "@mui/material/styles";
-import Tab from "@mui/material/Tab";
-import Tabs from "@mui/material/Tabs";
-import TextField from "@mui/material/TextField";
-import Tooltip from "@mui/material/Tooltip";
 import "./Homepage.css";
-import { isWithinInterval, subDays } from "date-fns";
-import { resolve } from "inversify-react";
 import { RouteComponentProps, StaticContext } from "react-router";
 
-import { AbsencesEnum, leaveTypesWithURLs, ViewsEnum } from "common/constants";
-import { DateUtil } from "common/DateUtil";
-import { IUserAbsenceWithEachDayStatus, IUserAbsenceWithWorkingDays } from "common/interfaces";
-import { HolidayDays } from "common/interfaces";
-import DeleteAbsenceModal from "components/common/DeleteAbsenceModal/DeleteAbsenceModal";
+import { AbsencesEnum, leaveTypesWithURLs } from "common/constants";
+import Absences from "components/Absences/Absences";
 import Error from "components/common/Error/Error";
-import { IAbsenceService, IHolidayService } from "inversify/interfaces";
-import { TYPES } from "inversify/types";
-
-const DateRangePickerDay = styled(MuiDateRangePickerDay, {
-  shouldForwardProp: (prop) => prop !== "isHighlighting" && prop !== "inlist",
-})(({ theme, isHighlighting, inlist, outsideCurrentMonth }) => {
-  return {
-    ...(isHighlighting &&
-      !inlist &&
-      !outsideCurrentMonth && {
-        borderRadius: "50%",
-        backgroundColor: theme.palette.primary.main,
-        color: theme.palette.common.white,
-        "&:hover, &:focus": {
-          backgroundColor: theme.palette.primary.dark,
-        },
-      }),
-    ...(inlist &&
-      !outsideCurrentMonth && {
-        backgroundColor: "lightblue",
-        borderRadius: "50%",
-      }),
-  };
-}) as React.ComponentType<DateRangePickerDayProps<Date>>;
-
-interface IUserAbcenseWithDate {
-  id: string;
-  type: string;
-  startingDate: Date;
-  endingDate: Date;
-  comment: string;
-  workingDays: number;
-  totalDays: number;
-}
 
 interface HomepageProps extends RouteComponentProps<null, StaticContext, { showSnackbar: boolean }> {}
 
@@ -82,17 +22,9 @@ interface HomepageState {
   error: string;
   successMessage: boolean;
   openSelectorDialog: boolean;
-  userPastAbsences: Array<IUserAbcenseWithDate>;
-  userFutureAbsences: Array<IUserAbcenseWithDate>;
-  view: ViewsEnum;
-  holidays: HolidayDays;
-  deleteAbsenceId: string;
 }
 
 class Homepage extends Component<HomepageProps, HomepageState> {
-  @resolve(TYPES.Absence) private absenceService!: IAbsenceService;
-  @resolve(TYPES.Holidays) private holidaysService!: IHolidayService;
-
   constructor(props: HomepageProps) {
     super(props);
     this.state = {
@@ -100,255 +32,22 @@ class Homepage extends Component<HomepageProps, HomepageState> {
       error: "",
       successMessage: false,
       openSelectorDialog: false,
-      userPastAbsences: [],
-      userFutureAbsences: [],
-      view: ViewsEnum.table,
-      holidays: [],
-      deleteAbsenceId: "",
     };
-  }
-
-  async componentDidMount(): Promise<void> {
-    this.openSnackbar(true);
-    this.setState({
-      loading: true,
-    });
-
-    await this.loadAbsences();
-    await this.loadHolidaysForThreeMonths();
-
-    this.setState({
-      loading: false,
-    });
-  }
-
-  async loadHolidaysForThreeMonths(): Promise<void> {
-    try {
-      const date = new Date();
-      const startingDate = new Date(date.getFullYear(), date.getMonth(), 1).toLocaleDateString("en-CA");
-      const endingDate = new Date(date.getFullYear(), date.getMonth() + 3, 1).toLocaleDateString("en-CA");
-      const holidays = await this.holidaysService.getDatesStatus({ startingDate, endingDate });
-      this.setState({
-        holidays,
-      });
-    } catch (error) {
-      this.setState({
-        error: error.message,
-      });
-    }
-  }
-
-  async loadAbsences(): Promise<void> {
-    try {
-      const userAbsences = await this.getUserAbsences();
-
-      this.setState({
-        userFutureAbsences: userAbsences.userFutureAbsences,
-        userPastAbsences: userAbsences.userPastAbsences,
-      });
-    } catch (error) {
-      this.setState({
-        error: error.message,
-      });
-    }
   }
 
   render(): JSX.Element {
-    if (this.state.error) {
-      return <Error message={this.state.error} />;
+    const { error } = this.state;
+    if (error) {
+      return <Error message={error} />;
     }
+
     return (
       <div className="homepage-root">
         <h1 className="homepage-header">My Absences</h1>
-        {this.state.userFutureAbsences.length === 0 && this.state.userPastAbsences.length === 0
-          ? this.renderNoUserAbsencesView()
-          : this.renderUserAbsencesTable()}
+        <Absences handleToggleSelectDialog={this.handleToggleSelectDialog} isShowingTeamAbsences={false} />
         {this.renderSnackbar()}
         {this.renderSelectDialog()}
-        {this.renderDeleteAbsenceConfirmationModal()}
       </div>
-    );
-  }
-
-  renderDeleteAbsenceConfirmationModal(): JSX.Element {
-    return (
-      <DeleteAbsenceModal
-        deleteAbsenceId={this.state.deleteAbsenceId}
-        handleCancelClick={this.handleCancelClick}
-        handleConfirmClick={this.handleConfirmClick}
-      />
-    );
-  }
-
-  renderAddAbsenceButton(): JSX.Element {
-    return (
-      <Button
-        className="homepage-add-absence-button"
-        onClick={() => this.handleToggleSelectDialog(true)}
-        variant="outlined"
-        color="primary"
-      >
-        Add absence
-      </Button>
-    );
-  }
-
-  renderUserAbsencesTable(): JSX.Element {
-    return (
-      <div>
-        <Grid container alignItems="center">
-          {this.renderAddAbsenceButton()}
-          {this.renderTabs()}
-        </Grid>
-        {this.renderHeaderAndFooter(true)}
-        {this.renderSeparator()}
-        {this.renderHeaderAndFooter(false)}
-        {this.renderAddAbsenceButton()}
-      </div>
-    );
-  }
-
-  renderTabs(): JSX.Element {
-    const { view } = this.state;
-
-    return (
-      <Grid item style={{ marginLeft: "30px" }}>
-        <Tabs indicatorColor="secondary" value={view} onChange={this.handleTabChange}>
-          <Tab value={ViewsEnum.table} label={"Table View"} />
-          <Tab value={ViewsEnum.calendar} label={"Calendar View"} />
-        </Tabs>
-      </Grid>
-    );
-  }
-
-  renderHeaderAndFooter(header: boolean): JSX.Element {
-    const { view } = this.state;
-    if (view === ViewsEnum.table) {
-      return this.renderTables(header);
-    } else if (header && view === ViewsEnum.calendar) {
-      return this.renderCalendar();
-    }
-    return <></>;
-  }
-
-  renderTables(header: boolean): JSX.Element {
-    return (
-      <TableContainer component={Paper}>
-        <Table aria-label="simple table">
-          {header ? (
-            <>
-              <TableHead>{this.renderTableHeaderAndFooterCells()}</TableHead>
-              <TableBody>{this.state.userFutureAbsences.map(this.mappingFunc)}</TableBody>
-            </>
-          ) : (
-            <>
-              <TableBody>{this.state.userPastAbsences.map(this.mappingFunc)}</TableBody>
-              <TableFooter className="homepage-table-footer">{this.renderTableHeaderAndFooterCells()}</TableFooter>
-            </>
-          )}
-        </Table>
-      </TableContainer>
-    );
-  }
-
-  getPeriods(): Array<{ start: Date; end: Date }> {
-    const mappingFunc = (absence: IUserAbcenseWithDate) => {
-      // In order to correctly display the period, you need to pass
-      // the day before the startingDate to the DateRangePicker component from Material UI
-      const start = subDays(absence.startingDate, 1);
-
-      return {
-        start,
-        end: absence.endingDate,
-      };
-    };
-
-    const futurePeriods = this.state.userFutureAbsences.map(mappingFunc);
-    const pastPeriods = this.state.userPastAbsences.map(mappingFunc);
-
-    return futurePeriods.concat(pastPeriods);
-  }
-
-  renderCalendar(): JSX.Element {
-    const periods = this.getPeriods();
-
-    return (
-      <StaticDateRangePicker
-        displayStaticWrapperAs="desktop"
-        calendars={3}
-        disablePast
-        disableFuture
-        disableHighlightToday
-        shouldDisableDate={() => true}
-        label="date range"
-        value={[null, null]}
-        onChange={() => null}
-        renderDay={(date: Date, dateRangePickerDayProps: DateRangePickerDayProps<Date>) => {
-          return this.renderAbsenceDay(date, dateRangePickerDayProps, periods);
-        }}
-        renderInput={(startProps, endProps) => (
-          <React.Fragment>
-            <TextField {...startProps} />
-            <Box sx={{ mx: 2 }}> to </Box>
-            <TextField {...endProps} />
-          </React.Fragment>
-        )}
-      />
-    );
-  }
-
-  renderAbsenceDay(
-    date: Date,
-    dateRangePickerDayProps: DateRangePickerDayProps<Date>,
-    periods: Array<{ start: Date; end: Date }>,
-    absences?: Array<IUserAbsenceWithEachDayStatus>,
-  ): JSX.Element {
-    const dayIsBetween = periods.some((p) => isWithinInterval(date, { start: p.start, end: p.end }));
-    const dayIsHoliday = this.checkIfDayIsHoliday(date);
-    const className = dayIsBetween && !dayIsHoliday ? "absence" : "non-absence";
-    dateRangePickerDayProps.inlist = dayIsHoliday;
-    dateRangePickerDayProps.isHighlighting = dayIsBetween;
-
-    if (absences && dayIsBetween && !dayIsHoliday && !dateRangePickerDayProps.outsideCurrentMonth) {
-      const names = this.getEmployeesNames(date, absences);
-
-      return (
-        <Tooltip title={names.toString()} arrow>
-          <Badge overlap="circular" color="secondary" badgeContent={names.length}>
-            <DateRangePickerDay className={className} {...dateRangePickerDayProps} />
-          </Badge>
-        </Tooltip>
-      );
-    }
-
-    return <DateRangePickerDay className={className} {...dateRangePickerDayProps} />;
-  }
-
-  renderTableHeaderAndFooterCells(): JSX.Element {
-    return (
-      <TableRow>
-        <TableCell width="8%" align="left">
-          <b>Type</b>
-        </TableCell>
-        <TableCell width="8%" align="left">
-          <b>From</b>
-        </TableCell>
-        <TableCell width="8%" align="left">
-          <b>To</b>
-        </TableCell>
-        <TableCell width="8%" align="left">
-          <b>Working days</b>
-        </TableCell>
-        <TableCell width="8%" align="left">
-          <b>Total days</b>
-        </TableCell>
-        <TableCell width="8%" align="left"></TableCell>
-        <TableCell width="8%" align="left"></TableCell>
-        <TableCell width="25%" align="left">
-          <b>Comment</b>
-        </TableCell>
-        <TableCell width="8%" align="left"></TableCell>
-      </TableRow>
     );
   }
 
@@ -380,39 +79,6 @@ class Homepage extends Component<HomepageProps, HomepageState> {
     );
   }
 
-  renderSeparator(): JSX.Element {
-    const { userFutureAbsences, userPastAbsences, view } = this.state;
-
-    if (!userFutureAbsences.length || !userPastAbsences.length || view === ViewsEnum.calendar) return <></>;
-    return (
-      <div className="or-spacer">
-        <div className="mask"></div>
-        <span>
-          <i>now</i>
-        </span>
-      </div>
-    );
-  }
-
-  renderNoUserAbsencesView(): JSX.Element {
-    if (this.state.loading) {
-      return <CircularProgress />;
-    }
-    return (
-      <div>
-        <div className="homepage-message-wrapper">
-          <Typography className="homepage-message-text" variant="h5" gutterBottom>
-            Looks like you need a break
-          </Typography>
-          <SentimentSatisfiedSharpIcon fontSize="large" />
-        </div>
-        <Button onClick={() => this.handleToggleSelectDialog(true)} variant="outlined" color="primary">
-          REQUEST ABSENCE
-        </Button>
-      </div>
-    );
-  }
-
   renderSnackbar(): JSX.Element {
     return (
       <Snackbar open={this.state.successMessage} onClose={() => this.openSnackbar(false)} autoHideDuration={2000}>
@@ -421,102 +87,11 @@ class Homepage extends Component<HomepageProps, HomepageState> {
     );
   }
 
-  private mappingFunc = (el: IUserAbcenseWithDate): JSX.Element => (
-    <TableRow hover key={el.id}>
-      <TableCell width="8%" align="left">
-        {el.type}
-      </TableCell>
-      <TableCell width="8%" align="left">
-        {el.startingDate.toLocaleDateString("en-CA")}
-      </TableCell>
-      <TableCell width="8%" align="left">
-        {el.endingDate.toLocaleDateString("en-CA")}
-      </TableCell>
-      <TableCell width="8%" align="left">
-        {el.workingDays}
-      </TableCell>
-      <TableCell width="8%" align="left">
-        {el.totalDays}
-      </TableCell>
-      <TableCell width="8%" align="left">
-        <Button color="primary" onClick={() => this.props.history.push(`/absence/${el.id}`)}>
-          view
-        </Button>
-      </TableCell>
-      <TableCell width="8%" align="left">
-        <Button color="primary" onClick={() => this.handleEditClick(el.id, el.type)}>
-          edit
-        </Button>
-      </TableCell>
-      <TableCell width="25%" align="left">
-        {el.comment ? el.comment : <div className="homepage-absence-comment">not available</div>}
-      </TableCell>
-      <TableCell width="8%" align="left">
-        <Button color="secondary" onClick={() => this.handleDeleteClick(el.id)}>
-          delete
-        </Button>
-      </TableCell>
-    </TableRow>
-  );
-
-  handleTabChange = (event: React.SyntheticEvent, newView: ViewsEnum): void => {
-    this.setState({
-      view: newView,
-    });
-  };
-
-  handleToggleSelectDialog(state: boolean): void {
+  handleToggleSelectDialog = (state: boolean): void => {
     this.setState({
       openSelectorDialog: state,
     });
-  }
-
-  handleDeleteClick(absenceId: string): void {
-    this.setState({
-      deleteAbsenceId: absenceId,
-    });
-  }
-
-  handleCancelClick = (): void => {
-    this.setState({
-      deleteAbsenceId: "",
-    });
   };
-
-  handleConfirmClick = async (currentAbsenceId: string): Promise<void> => {
-    try {
-      await this.absenceService.deleteAbsence(currentAbsenceId);
-
-      const pastAbsencesWithoutDeletedOne = this.state.userPastAbsences.filter(
-        (absence) => absence.id !== currentAbsenceId,
-      );
-
-      const futureAbsencesWithoutDeletedOne = this.state.userFutureAbsences.filter(
-        (absence) => absence.id !== currentAbsenceId,
-      );
-
-      this.setState({
-        userPastAbsences: pastAbsencesWithoutDeletedOne,
-        userFutureAbsences: futureAbsencesWithoutDeletedOne,
-        deleteAbsenceId: "",
-      });
-    } catch (error) {
-      this.setState({
-        error: error.message,
-      });
-    }
-  };
-
-  handleEditClick(currentAbsenceId: string, type: string): void {
-    const absenceUrl = Object.values(leaveTypesWithURLs).find((absence) => absence.leave === type);
-    if (!absenceUrl) {
-      this.setState({
-        error: "Selected type is not supported",
-      });
-      return;
-    }
-    this.props.history.push(`/edit/${absenceUrl.url}/${currentAbsenceId}`);
-  }
 
   openSnackbar(isOpen: boolean): void {
     if (this.props.location.state?.showSnackbar && isOpen) {
@@ -529,56 +104,6 @@ class Homepage extends Component<HomepageProps, HomepageState> {
         successMessage: false,
       });
     }
-  }
-
-  async getUserAbsences(): Promise<{
-    userFutureAbsences: Array<IUserAbcenseWithDate>;
-    userPastAbsences: Array<IUserAbcenseWithDate>;
-  }> {
-    const userAbsences = await this.absenceService.getUserAbsences();
-    const userFutureAbsences = this.filterAndMapUserAbsences(
-      userAbsences,
-      (el) => el.startingDate > DateUtil.todayStringified(),
-    );
-    const userPastAbsences = this.filterAndMapUserAbsences(
-      userAbsences,
-      (el) => el.startingDate <= DateUtil.todayStringified(),
-    );
-
-    return {
-      userFutureAbsences,
-      userPastAbsences,
-    };
-  }
-
-  filterAndMapUserAbsences(
-    absences: Array<IUserAbsenceWithWorkingDays>,
-    filterFunc: (absence: IUserAbsenceWithWorkingDays) => boolean,
-  ): Array<IUserAbcenseWithDate> {
-    return absences
-      .filter(filterFunc)
-      .sort(DateUtil.dateSorting)
-      .map((a) => ({ ...a, startingDate: new Date(a.startingDate), endingDate: new Date(a.endingDate) }));
-  }
-
-  getEmployeesNames(date: Date, absences: Array<IUserAbsenceWithEachDayStatus>): Array<string> {
-    return absences
-      .filter((absence) => {
-        const start = subDays(new Date(absence.startingDate), 1);
-        const end = new Date(absence.endingDate);
-
-        return isWithinInterval(date, { start, end });
-      })
-      .map((a) => a.employee.firstName);
-  }
-
-  checkIfDayIsHoliday(date: Date): boolean {
-    const holiday = this.state.holidays.find((holiday) => holiday.date === date.toLocaleDateString("en-CA"));
-    if (holiday) {
-      return holiday.status !== "workday";
-    }
-
-    return false;
   }
 }
 
