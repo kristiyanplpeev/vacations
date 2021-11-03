e8f63e1cf229029c63933c5b806e1a47699656d7
Ivelin Stoyanov <ivelin.stoyanov@atscale.com>
2021-11-03 17:38:37 +0200

    Add Teams component

--- client/src/components/Teams/Teams.tsx
+++ client/src/components/Teams/Teams.tsx
@@ -1,18 +1,41 @@
 import React, { Component } from "react";
 
-import { Grid, Typography, Stack, Paper, Container, Button } from "@mui/material";
+import AddIcon from "@mui/icons-material/Add";
+import DeleteIcon from "@mui/icons-material/Delete";
+import {
+  Grid,
+  Typography,
+  Stack,
+  Paper,
+  Container,
+  Button,
+  Chip,
+  Avatar,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+  TextField,
+  Divider,
+} from "@mui/material";
 import { resolve } from "inversify-react";
 
-import { anyPosition, anyTeam } from "common/constants";
+import { anyPosition, anyRole, anyTeam, TeamsEnum } from "common/constants";
 import { ITeams, IUserWithTeamAndPosition } from "common/interfaces";
+import Error from "components/common/Error/Error";
 import { IUserService } from "inversify/interfaces";
 import { TYPES } from "inversify/types";
+import "./Teams.css";
 
 export interface TeamsProps {}
 
 export interface TeamsState {
   loading: boolean;
   error: string;
+  open: boolean;
+  teamName: string;
+  teamNameError: string;
   teams: Array<ITeams>;
   users: Array<IUserWithTeamAndPosition>;
 }
@@ -26,6 +49,9 @@ class Teams extends Component<TeamsProps, TeamsState> {
     this.state = {
       loading: false,
       error: "",
+      open: false,
+      teamName: "",
+      teamNameError: "",
       teams: [],
       users: [],
     };
@@ -60,7 +86,7 @@ class Teams extends Component<TeamsProps, TeamsState> {
 
   async loadUsers(): Promise<void> {
     try {
-      const users = await this.userService.getAllUsers(anyTeam, anyPosition);
+      const users = await this.userService.getAllUsers(anyTeam, anyPosition, anyRole);
 
       this.setState({
         users,
@@ -73,67 +99,209 @@ class Teams extends Component<TeamsProps, TeamsState> {
   }
 
   render(): JSX.Element {
+    const { error } = this.state;
+    if (error) {
+      return <Error message={error} />;
+    }
     return (
-      <Container>
+      <Container className="teams-page-container">
         <Typography variant="h2">Teams</Typography>
-        <Grid container>{this.renderTeams()}</Grid>
-        {this.renderUsers()}
+        {this.renderAddTeam()}
+        {this.renderTeams()}
+        {this.renderUsersWithoutTeam()}
       </Container>
+    );
+  }
+
+  renderAddTeam(): JSX.Element {
+    const { open, teamName, teamNameError } = this.state;
+    return (
+      <>
+        <Button startIcon={<AddIcon />} onClick={this.handleClickOpen} variant="outlined">
+          Add Team
+        </Button>
+        <Dialog open={open} onClose={this.handleClose}>
+          <DialogTitle>Add Team</DialogTitle>
+          <DialogContent>
+            <DialogContentText>Please enter the name of the team!</DialogContentText>
+            <TextField
+              autoFocus
+              margin="dense"
+              value={teamName}
+              fullWidth
+              variant="standard"
+              onChange={this.handleTeamNameChange}
+              error={!!teamNameError}
+              helperText={teamNameError}
+            />
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={this.handleClose}>Cancel</Button>
+            <Button onClick={this.handleSubmit}>Submit</Button>
+          </DialogActions>
+        </Dialog>
+      </>
     );
   }
 
   renderTeams(): JSX.Element {
     return (
-      <>
+      <Grid container>
         {this.state.teams.map((team) => (
-          <Paper key={team.id} style={{ height: "800px" }}>
-            <Typography variant="h3">{team.team}</Typography>
-            {this.renderUsers(team.team)}
-          </Paper>
+          <Grid key={team.id} item xs={6}>
+            <Paper variant="outlined" className="team-container">
+              <Typography variant="h4">{team.team}</Typography>
+              <Divider className="divider" />
+              {this.renderUsers(team)}
+            </Paper>
+          </Grid>
         ))}
-      </>
+      </Grid>
     );
   }
 
-  renderUsers(teamName?: string): JSX.Element {
-    if (teamName) {
-      const filteredUsers = this.state.users.filter((u) => u.team === teamName);
-      console.log(filteredUsers);
+  // eslint-disable-next-line max-lines-per-function
+  renderUsers(team?: ITeams): JSX.Element {
+    if (team) {
+      const filteredUsers = this.state.users.filter((u) => u.team === team.team);
       return (
         <Stack spacing={2}>
           {filteredUsers.length > 0 ? (
             <>
               {filteredUsers.map((user) => (
-                <Grid container key={user.id} justifyContent="space-between">
-                  <Grid item>position - todo</Grid>
+                <Grid container key={user.id} justifyContent="space-between" alignItems="center">
+                  <Grid item>
+                    <Chip
+                      className="teams-card-chip"
+                      avatar={<Avatar className="teams-card-chip-avatar" alt={user.firstName[0]} src={user.picture} />}
+                      label={user.position}
+                      variant="outlined"
+                    />
+                  </Grid>
                   <Grid item>{user.email}</Grid>
                 </Grid>
               ))}
             </>
           ) : (
-            <>
+            <Stack spacing={8} className="empty-team">
               <Typography variant="h5">---no employees---</Typography>
-              <Button>Delete Team</Button>
-            </>
+              <Button
+                startIcon={<DeleteIcon />}
+                variant="contained"
+                className="delete-team-button"
+                onClick={() => this.handleDelete(team.id)}
+              >
+                Delete Team
+              </Button>
+            </Stack>
           )}
         </Stack>
       );
     } else {
-      const usersWithoutTeam = this.state.users.filter((u) => u.team === "no team");
-      return (
-        <>
-          <Typography variant="h3">Users without a team</Typography>
-          <Grid container>
-            {usersWithoutTeam.map((user) => (
-              <Grid item key={user.id}>
-                {user.email}
-              </Grid>
-            ))}
-          </Grid>
-        </>
-      );
+      return this.renderUsersWithoutTeam();
     }
   }
+
+  renderUsersWithoutTeam(): JSX.Element {
+    const usersWithoutTeam = this.state.users.filter((u) => u.team === TeamsEnum.noTeam);
+    return (
+      <Stack spacing={2}>
+        <Typography variant="h3" align="left">
+          Users without a team
+        </Typography>
+        <Grid container spacing={2} className="users-without-team" alignContent="center" alignItems="center">
+          {usersWithoutTeam.map((user) => (
+            <Grid item key={user.id} xs={4}>
+              <Chip
+                className="teams-card-chip"
+                avatar={<Avatar className="teams-card-chip-avatar" alt={user.firstName[0]} src={user.picture} />}
+                label={user.email}
+                variant="outlined"
+              />
+            </Grid>
+          ))}
+        </Grid>
+      </Stack>
+    );
+  }
+
+  handleTeamNameChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
+    const { value } = event.target;
+    this.setState({
+      teamName: value,
+    });
+  };
+
+  checkForTeamNameError = (): boolean => {
+    const { teams, teamName } = this.state;
+    if (!teamName || teamName.length < 3 || teamName.length > 50) {
+      this.setState({
+        teamNameError: "Please enter a name between 3 and 50 characters",
+      });
+
+      return true;
+    }
+
+    const teamNames = teams.map((t) => t.team);
+    const nameAlreadyExists = teamNames.some((team) => team.toLowerCase().trim() === teamName.toLowerCase().trim());
+    if (nameAlreadyExists) {
+      this.setState({
+        teamNameError: "This team name already exists!",
+      });
+
+      return true;
+    }
+
+    return false;
+  };
+
+  handleDelete = async (teamId: string): Promise<void> => {
+    try {
+      await this.userService.deleteTeam(teamId);
+
+      const remainingTeams = this.state.teams.filter((t) => t.id !== teamId);
+      this.setState({
+        teams: remainingTeams,
+      });
+    } catch (e) {
+      this.setState({
+        error: e.message,
+      });
+    }
+  };
+
+  handleSubmit = async (): Promise<void> => {
+    try {
+      const hasError = this.checkForTeamNameError();
+      if (hasError) {
+        return;
+      }
+
+      const newTeam = await this.userService.postTeam(this.state.teamName);
+      this.setState({
+        teams: [...this.state.teams, newTeam],
+      });
+      this.handleClose();
+    } catch (e) {
+      this.setState({
+        error: e.message,
+      });
+    }
+  };
+
+  handleClickOpen = (): void => {
+    this.setState({
+      open: true,
+    });
+  };
+
+  handleClose = (): void => {
+    this.setState({
+      open: false,
+      teamName: "",
+      teamNameError: "",
+    });
+  };
 }
 
 export default Teams;
